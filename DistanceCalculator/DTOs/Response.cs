6ff7bd1d1b561051995b052b29cf112f4cd59935
mhsohail <sohailx2x@yahoo.com>
2015-07-22 00:38:22 +0500

    untracked

---
+++ DistanceCalculator/DTOs/Response.cs
@@ -0,0 +1,15 @@
+﻿using DistanceCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DistanceCalculator.DTOs
+{
+    public class Response
+    {
+        public bool IsSucceed { get; set; }
+        public string Message { get; set; }
+        public ICollection<CalculatedMsa> CalculatedMsas { get; set; }
+    }
+}
